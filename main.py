1549418d7b9ea49796f684f8e27ea63dc137da20
viniciusucanutx <viniciuscanutmz@gmail.com>
2024-10-24 13:04:35 -0300

    :lipstick: feat: Estilização da interface com flet e definindo o tamanho da janela.

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import flet as ft 
-import sqlite3
-from sqlite3 import Error
 import random 
 import string
 import db.queries
+import functions.functions
+
+def main(page: ft.Page):
+    
+    page.title="Password Generator"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
+    page.spacing = 20
+    page.window_width = 330
+    page.window_height = 600
+    
+    logged_in_email = None
+    password_length_dropdown = ft.Ref[ft.Dropdown]()
+    password_source_input = ft.Ref[ft.TextField]()
 
 
-
-
-
+ft.app(target=main)
