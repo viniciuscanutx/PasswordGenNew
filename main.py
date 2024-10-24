2114e4aeb7d0b869e52d1a36e3ca248040c7f4f0
viniciusucanutx <viniciuscanutmz@gmail.com>
2024-10-24 12:44:29 -0300

    :wrench: chore: Acesso do front a Database

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ import sqlite3
 from sqlite3 import Error
 import random 
 import string
+
+db_account = './db/users.db'
+db_passwords = './db/passwords.db'
+
+def acess_db(db_name):
+    try:
+        conn=sqlite3.connect(db_name)
+        return conn
+    except Error as e:
+        print(e)
+    return None
